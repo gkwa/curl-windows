0c24877096a43447c0a0fc5b11d2b2cd84e78be7
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-04 22:12:17 -0800

    flailing

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,10 +6,23 @@
 
 Chef::Resource::RemoteFile.send(:include, CurlWindows::Helper)
 
-
 url = node['curl']['url']
+app_dir = "c:\\Program Files\\Curl\\"
 
-remote_file "#{Chef::Config['file_cache_path']}\\#{filename_from_url(url)}" do
 
+execute 'extract_project' do
+  command "tar xfvz #{Chef::Config[:file_cache_path]}\\#{filename_from_url(url)}"
+  # cwd base_dir
+  # action (File.exist?(app_dir) ? :nothing : :run)
+  # subscribes :run, "zipfile_download"
+  # notifies :create, 'remote_file[zipfile_download]', :immediately
+  action :nothing
 end
 
+remote_file "zipfile_download" do
+  path "#{Chef::Config['file_cache_path']}\\#{filename_from_url(url)}"
+  source url
+  use_last_modified true
+  use_conditional_get true
+  notifies :run, 'execute[extract_project]', :immediately
+end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,11 +2,98 @@ require 'spec_helper'
 
 describe 'curl-windows::default' do
 
-  let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'windows', version: '2012').converge('curl-windows::default') }
+  require 'chefspec'
 
-  it 'downloads curl zip' do
-    expect(chef_run).to create_remote_file("#{Chef::Config['file_cache_path']}\\curl-7.51.0-win32-VC14.zip")
+  describe 'curl-windows::default' do
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2012').converge(described_recipe) }
+
+    # it 'deletes a file with an explicit action' do
+    #   expect(chef_run).to delete_file('/tmp/explicit_action')
+    #   expect(chef_run).to_not delete_file('/tmp/not_explicit_action')
+    # end
+    #
+    # it 'deletes a file with attributes' do
+    #   expect(chef_run).to delete_file('/tmp/with_attributes').with(backup: false)
+    #   expect(chef_run).to_not delete_file('/tmp/with_attributes').with(backup: true)
+    # end
+
+    it 'downloads zipfile' do
+      url = 'https://www.apachelounge.com/download/VC14/binaries/curl-7.51.0-win32-VC14.zip'
+      # expect(chef_run).to create_remote_file("#{Chef::Config['file_cache_path']}\\#{filename_from_url(url)}")
+      expect(chef_run).to create_remote_file('zipfile_download').with(use_conditional_get: true)
+    end
+
+    it 'notifies the extraction' do
+      remote_file = chef_run.remote_file('zipfile_download')
+      expect(remote_file).to notify('execute[extract_project]').to(:run)
+    end
+
+    # it 'downloads the remote_file only if it\'s changed' do
+    #   expect(chef_run).to create_remote_file('codebase_download').with(use_conditional_get: true)
+    # end
+
   end
+
+  #
+  # context 'windows' do
+  #   let(:chef_run) do
+  #     runner = ChefSpec::SoloRunner.new(
+  #         'platform' => 'windows',
+  #         'version' => '2008R2'
+  #     )
+  #     runner.node.set['culr']['url'] = 'https://www.apachelounge.com/download/VC14/binaries/curl-7.51.0-win32-VC14.zip'
+  #     runner.converge('curl-windows::default')
+  #   end
+  #
+  #   it 'should include the windows recipe' do
+  #     # chef_run.should include_recipe 'java::windows'
+  #     # chef_run.should create_remote_file "c:\\windows\\temp\\curl-7.51.0-win32-VC14.zip"
+  #
+  #   end
+  # end
+
+
+  # let (:chef_run) { ChefSpec::ChefRunner.new.converge('curl-windows::default') }
+  #
+  # context 'windows' do
+  #   let(:chef_run) do
+  #     runner = ChefSpec::SoloRunner.new(
+  #         'platform' => 'windows',
+  #         'version' => '2008R2'
+  #     )
+  #     runner.node.set['culr']['url'] = 'https://www.apachelounge.com/download/VC14/binaries/curl-7.51.0-win32-VC14.zip'
+  #     runner.converge('curl-windows::default')
+  #   end
+  #
+  #   it 'should include the windows recipe' do
+  #     # chef_run.should include_recipe 'java::windows'
+  #     chef_run.should create_remote_file "c:\\windows\\temp\\curl-7.51.0-win32-VC14.zip"
+  #
+  #   end
+  # end
+
+
+
+
+
+  # before do
+  #   Fauxhai.mock(platform: 'windows', version: '2012') do |node|
+  #     node['curl']['url'] = 'https://www.apachelounge.com/download/VC14/binaries/curl-7.51.0-win32-VC14.zip'
+  #   end
+  # end
+  #
+  # let(:chef_run) { ChefSpec::ChefRunner.new.converge('curl-windows::default') }
+  #
+  # it 'fetches zip file' do
+  #   chef_run.should create_remote_file "c:\\windows\\temp\\curl-7.51.0-win32-VC14.zip"
+  # end
+
+
+  # it 'downloads curl zip' do
+  #   @runner = ChefSpec::ChefRunner.new.converge('curl-windows::default')
+  #   # @runner = expect(chef_run).to create_remote_file("c:\\windows\\temp\\curl-7.51.0-win32-VC14.zip")
+  #   @runner.should create_file("c:\\windows\\temp\\curl-7.51.0-win32-VC14.zip")
+  # end
 
   it 'unzips curl zip file'
   it 'installs curl.exe'
