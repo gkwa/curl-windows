4753dc333bc51371f8874faa30ebd0cb4d91dc75
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-04 23:50:04 -0800

    cleanup after flail
    t

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
-
-
 default['curl']['url'] = 'https://www.apachelounge.com/download/VC14/binaries/curl-7.51.0-win32-VC14.zip'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,25 +4,13 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-Chef::Resource::RemoteFile.send(:include, CurlWindows::Helper)
-
-url = node['curl']['url']
-app_dir = "c:\\Program Files\\Curl\\"
-
 execute 'extract_project' do
-  command "tar xfvz #{Chef::Config[:file_cache_path]}\\#{filename_from_url(url)}"
-  # cwd base_dir
-  # action (File.exist?(app_dir) ? :nothing : :run)
-  # subscribes :run, "zipfile_download"
-  # notifies :create, 'remote_file[zipfile_download]', :immediately
+  command "tar xfvz #{Chef::Config[:file_cache_path]}\\#{filename_from_url(node['curl']['url'])}"
   action :nothing
 end
 
-
 remote_file "zipfile_download" do
-  path "#{Chef::Config['file_cache_path']}\\#{filename_from_url(url)}"
-  source url
-  use_last_modified true
-  use_conditional_get true
+  path "#{Chef::Config['file_cache_path']}\\#{filename_from_url(node['curl']['url'])}"
+  source node['curl']['url']
   notifies :run, 'execute[extract_project]', :immediately
 end
